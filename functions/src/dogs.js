61cbc379b5f77811a8e33b30429cc72f50756dff
Ariole Dieujuste <arioledieujuste.com>
2022-08-23 02:25:12 -0400

    express deployed

---
+++ functions/src/dogs.js
@@ -0,0 +1,11 @@
+export function getDogs(req, res) {
+  res.send([
+    { name: 'Ryder', age: 4, breed: 'Dumbass', gender: 'MN' },
+    { name: 'Duke', age: 2, breed: 'Rotweiler', gender: 'M' },
+    { name: 'Indigo', age: 10, breed: 'Great Dane / Lab Mix', gender: 'MN' },
+  ]);
+}
+
+export function testApi(req, res) {
+  res.send({ success: true, message: 'API is working!' });
+}
